7a7650b321e34263bab42355ec4242a52fe4583c
klauguima <klauguima@gmail.com>
2025-08-26 20:00:55 -0300

    recompilando

--- Heranca/heranca/Program.java
+++ Heranca/heranca/Program.java
@@ -8,7 +8,7 @@ public class Program {
 
         System.out.println("meuAnimal eh um " + meuAnimal.getClass().getName());
         System.out.println("meuCachorro eh um " + meuCachorro.getClass().getName());
-        System.out.println("objeto eh um " + obj.getClass().getName());
+        System.out.println("obj eh um " + obj.getClass().getName());
         System.out.println("umOutroAnimal eh um " + umOutroAnimal.getClass().getName());
     }
 }
