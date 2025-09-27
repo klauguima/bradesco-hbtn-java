e432d8116fa07c79a2e43aea2bb43a88d5f226b2
klauguima <klauguima@gmail.com>
2025-09-27 00:23:33 -0300

    exercicios exceptions

---
+++ 0x03/conta_basica/ContaBancariaBasica.java
@@ -0,0 +1,61 @@
+import exceptions.OperacaoInvalidaException;
+
+public class ContaBancariaBasica {
+    private String numeracao;
+    private double saldo;
+    private double taxaJurosAnual;
+
+    public ContaBancariaBasica(String numeracao, double taxaJurosAnual) {
+        this.numeracao = numeracao;
+        this.taxaJurosAnual = taxaJurosAnual;
+        this.saldo = 0.0;
+    }
+
+    public String getNumeracao() {
+        return numeracao;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public double getTaxaJurosAnual() {
+        return taxaJurosAnual;
+    }
+
+    public void depositar(double valor) throws OperacaoInvalidaException {
+        if (valor <= 0) {
+            throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+        }
+        this.saldo += valor;
+    }
+
+    public void sacar(double valor) throws OperacaoInvalidaException {
+        if (valor <= 0) {
+            throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
+        }
+        if (valor > this.saldo) {
+            throw new OperacaoInvalidaException("Valor de saque deve ser menor que o saldo atual");
+        }
+        this.saldo -= valor;
+    }
+
+    public double calcularTarifaMensal() {
+        double dezPorCento = this.saldo * 0.10;
+        return Math.min(10.0, dezPorCento);
+    }
+
+    public double calcularJurosMensal() {
+        if (this.saldo < 0) {
+            return 0.0;
+        }
+        double taxaMensal = (this.taxaJurosAnual / 12.0) / 100.0;
+        return this.saldo * taxaMensal;
+    }
+
+    public void aplicarAtualizacaoMensal() {
+        double tarifa = calcularTarifaMensal();
+        double juros = calcularJurosMensal();
+        this.saldo = this.saldo - tarifa + juros;
+    }
+}
